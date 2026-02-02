41ca91344d3393dc27652e70c034ebf787c942b5
longcongchua <phamlongkh2006@gmail.com>
2026-02-02 10:35:13 +0700

    suon cua project

---
+++ src/main/java/com/example/CryptoTracking/CryptoTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.CryptoTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CryptoTracking/CryptoTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CryptoTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
